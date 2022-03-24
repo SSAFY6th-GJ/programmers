eaa9b8f12e2b012d28e64ff65db4b27d19818811
seongbiny <s30ngb1n@gmail.com>
2022-03-24 21:17:04 +0900

    create: 없는숫자더하기_성빈

---
+++ 없는 숫자 더하기/없는숫자더하기_성빈.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = -1
+    answer = 45 - sum(numbers)
+    return answer
+
+'''
+0~9 까지 총 합 = 45
+numbers 배열의 총 합을 빼주면 됨
+
+def solution(numbers):
+    return 45 - sum(numbers)
+'''
